979f478ad9e81c2347f758a9bd498d453fc07532
Olga <b-oa@yandex.ru>
2023-08-03 00:44:56 +0300

    наала решать задачу 8

--- zadacha8/Program.cs
+++ zadacha8/Program.cs
@@ -3,3 +3,18 @@
 // а на выходе показывает все чётные числа от 1 до N.
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
+Console.Write("Введите число  ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int n=1;
+if (n < number1)
+    {
+        int half = n/2;
+        if (n == 2 * half) 
+    {
+        Console.Write($" {n} ");
+        n = n+1;
+    }
+    else 
+    {n = n+1;}
+    }
+ Console.Write("конец");
