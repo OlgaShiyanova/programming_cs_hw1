3af758f8266adc6370b7ae265bf1cc0b8afdfc99
Olga <b-oa@yandex.ru>
2023-08-03 00:11:57 +0300

    сделала задачу 6

--- zadacha5/Program.cs
+++ zadacha5/Program.cs
@@ -4,3 +4,15 @@
 // 4 -> да
 // -3 -> нет
 // 7 -> нет
+Console.Write("Введите число  ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int half = number1/2;
+if (number1 == 2 * half) 
+{
+    Console.Write("да");
+}
+else
+{
+   Console.Write("нет");
+}
+ 
