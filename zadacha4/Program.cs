a3b94e85639140a83b888840b5953dcb4a89218f
Olga <b-oa@yandex.ru>
2023-08-02 23:56:11 +0300

    made task 4

--- zadacha4/Program.cs
+++ zadacha4/Program.cs
@@ -4,3 +4,23 @@
 // 2, 3, 7 -> 7
 // 44 5 78 -> 78
 // 22 3 9 -> 22
+Console.Write("Введите 1 число ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2 число ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 3 число ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+if (number1 > number2) 
+{
+    max = number1;
+}
+else
+{
+    max = number2;
+}
+    if (number3>max)
+    {
+        max=number3;
+    }
+Console.Write($"число {max} большее"); 
